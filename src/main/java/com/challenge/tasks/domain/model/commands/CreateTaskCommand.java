56ea405c08f328c0ff53a90db832ca428cd596cf
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2026-01-16 02:28:45 -0500

    refactor(tasks): add status parameter to create task command.

--- src/main/java/com/challenge/tasks/domain/model/commands/CreateTaskCommand.java
+++ src/main/java/com/challenge/tasks/domain/model/commands/CreateTaskCommand.java
@@ -1,6 +1,7 @@
 package com.challenge.tasks.domain.model.commands;
 
 import com.challenge.tasks.domain.model.valueobjects.TaskPriority;
+import com.challenge.tasks.domain.model.valueobjects.TaskStatus;
 
 import java.time.LocalDate;
 
@@ -10,6 +11,7 @@ import java.time.LocalDate;
  * @param description The description of the task
  * @param priority The priority of the task
  * @param dueDate The due date of the task
+ * @param status The initial status of the task (optional, defaults to TODO)
  * @author Gonzalo Qu3dena
  * @version 1.0.0
  * @since 1.0.0
@@ -18,6 +20,7 @@ public record CreateTaskCommand(
   String title, 
   String description, 
   TaskPriority priority, 
-  LocalDate dueDate 
+  LocalDate dueDate,
+  TaskStatus status
 ) {
 }
